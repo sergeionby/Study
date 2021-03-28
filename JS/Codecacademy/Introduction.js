396c3a62258fe73c1e17b3a6447b799a197220a4
sergeionby <sergeionby@gmail.com>
2021-03-28 16:44:05 +0300

    JS: introduction, variables

---
+++ JS/Codecacademy/Introduction.js
@@ -0,0 +1,42 @@
+//  INTRODUCTION 2/10 - Console
+    console.log(2021-1988);
+
+//  INTRODUCTION 3/10 - Comments
+
+//   INTRODUCTION 4/10 - Data Types
+    In JavaScript, there are seven fundamental data types:
+        1) Number: Any number, including numbers with decimals: 4, 8, 1516, 23.42.
+        2) String: Any grouping of characters on your keyboard (letters, numbers, spaces, symbols, etc.) surrounded by single quotes: ' ... ' or double quotes " ... ". Though we prefer single quotes. Some people like to think of string as a fancy word for text.
+        3) Boolean: This data type only has two possible values— either true or false (without quotes). It’s helpful to think of booleans as on and off switches or as the answers to a “yes” or “no” question.
+        4) Null: This data type represents the intentional absence of a value, and is represented by the keyword null (without quotes).
+        5) Undefined: This data type is denoted by the keyword undefined (without quotes). It also represents the absence of a value though it has a different use than null.
+        6) Symbol: A newer feature to the language, symbols are unique identifiers, useful in more complex coding. No need to worry about these for now.
+        7) Object: Collections of related data.
+    The first 6 of those types are considered primitive data types. They are the most basic data types in the language. Objects are more complex. 
+
+        console.log('JavaScript');
+        console.log(2011);
+        console.log('Woohoo! I love to code! #codecademy');
+        console.log(20.49);
+
+//  INTRODUCTION 5/10 - Arithmetic Operators
+    console.log(33+3.5);
+    console.log(2021-1969);
+    console.log(65/240);
+    console.log(0.2708*100);
+
+//  INTRODUCTION 6/10 - String Concatenation
+    console.log('Hello'+'World');
+    console.log('Hello '+'World');
+
+//  INTRODUCTION 7/10 - Properties
+    console.log('Teaching the world how to code'.length);
+
+//  INTRODUCTION 8/10 - Methods
+    console.log('Codecademy'.toUpperCase());
+    console.log('    Remove whitespace   '.trim());
+
+//  INTRODUCTION 9/10 - Built-in Objects
+    console.log(Math.floor(Math.random()*100));
+    console.log(Math.ceil(43.8));
+    console.log(Number.isInteger(2017));
---
+++ JS/Codecacademy/Variables.js
@@ -0,0 +1,45 @@
+//  Variables 2/10 - Create a Variable: var
+    var favoriteFood='pizza';
+    var numOfSlices = 8;
+    console.log(favoriteFood);
+    console.log(numOfSlices);
+
+//  Variables 3/10 - Create a Variable: let
+    let changeMe = false;
+    console.log(changeMe);
+
+//  Variables 4/10 - Create a Variable: const
+    const entree = 'Enchiladas';
+    console.log(entree);
+
+//  Variables 5/10 - Mathematical Assignment Operators
+    let levelUp = 10;
+    let powerLevel = 9001;
+    let multiplyMe = 32;
+    let quarterMe = 1152;
+
+    levelUp +=5;
+    powerLevel -=100;
+    multiplyMe *=11;
+    quarterMe /=4;
+
+//  Variables 6/10 - The Increment and Decrement Operator
+    let gainedDollar = 3;
+    let lostDollar = 50;
+    gainedDollar++;
+    lostDollar--;
+
+//  Variables 7/10 - String Concatenation with Variables
+    let favoriteAnimal = 'Filia';
+    console.log('My favorite animal: '+favoriteAnimal);
+
+//  Variables 8/10 - String Interpolation
+    let myName = 'Sergei';
+    let myCity = 'Minsk'
+    console.log(`My name is ${myName}. My favorite city is ${myCity}`);
+
+//  Variables 9/10 - typeof operator
+    let newVariable = 'Playing around with typeof.';
+    console.log(typeof newVariable);
+    newVariable = 1;
+    console.log(typeof newVariable);
