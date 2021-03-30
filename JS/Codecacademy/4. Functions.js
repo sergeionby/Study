d727088c5d5df736af136a4adb7d5cb1eafdbde3
sergeionby <sergeionby@gmail.com>
2021-03-31 00:52:28 +0300

    update functions

--- JS/Codecacademy/4. Functions.js
+++ JS/Codecacademy/4. Functions.js
@@ -37,3 +37,37 @@ function monitorCount(rows, columns) {
     console.log(numOfMonitors);
 
 //  Helper Functions
+function monitorCount(rows, columns) {
+    return rows * columns;
+  }
+  
+  function costOfMonitors (rows, columns){
+    return monitorCount(rows, columns) * 200;
+  }
+  
+  const totalCost = costOfMonitors(5,4);
+  
+  console.log(totalCost);
+
+//  Function Expressions
+const plantNeedsWater = function (day) {
+    if (day === 'Wednesday') {
+      return true;
+    } else {
+      return false;
+    }
+   }
+   
+   console.log(plantNeedsWater('Tuesday'));
+
+//   Arrow Functions
+const plantNeedsWater = (day) => {
+    if (day === 'Wednesday') {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+//    Concise Body Arrow Functions
+const plantNeedsWater = day => day === 'Wednesday' ? true : false;
